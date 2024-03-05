e48c2429ab19efb43f88987b411e0698f002e254
miruetoto <miruetoto@gmail.com>
2024-03-05 11:50:02 +0900

    render

---
+++ index.jl
@@ -0,0 +1,49 @@
+### A Pluto.jl notebook ###
+# v0.19.40
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 9ba3fd28-a02f-11ec-2d00-417ff6fef7d3
+md"""
+# 2024년 통계전산 강의홈페이지 
+"""
+
+# ╔═╡ f589bb03-2bf2-4f65-b351-0e3c53eae11b
+md"""
+### Lectures
+"""
+
+# ╔═╡ 1f7b4001-be12-44dc-8745-c45ed7d2ad03
+md"""
+---
+-- Julia 프로그래밍
+- [01wk-1](https://guebin.github.io/SC2024/01wk-01.html)
+
+-- 수리통계학
+
+-- 선형대수학
+"""
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─9ba3fd28-a02f-11ec-2d00-417ff6fef7d3
+# ╟─f589bb03-2bf2-4f65-b351-0e3c53eae11b
+# ╟─1f7b4001-be12-44dc-8745-c45ed7d2ad03
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
