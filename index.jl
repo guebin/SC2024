4610e2bbdb65438e43110743f7a90e3140a47ee3
miruetoto <miruetoto@gmail.com>
2024-03-19 12:41:35 +0900

    .

--- index.jl
+++ index.jl
@@ -30,7 +30,8 @@ md"""
 # ╔═╡ 3ab7086d-a534-449f-a914-fe314dc0a4a1
 md"""
 -- 수리통계학
-- [02wk-2](https://guebin.github.io/SC2024/02wk-1.html): 베르누이분포, 이항분포, 포아송분포
+- [02wk-1](https://guebin.github.io/SC2024/02wk-1.html): 베르누이분포, 이항분포, 포아송분포
+- [03wk-1](https://guebin.github.io/SC2024/03wk-1.html): 지수분포, inverse cdf, 박스뮬러변환
 """
 
 # ╔═╡ 22307a85-f60c-4888-b4ef-afb1d09d0253
